de26bb7f3b6227819a62f1bcf023a00da09332d0
Alex Sousa <lexusmem@gmail.com>
2024-07-24 21:33:09 -0300

    novas aulas python developer

---
+++ Formação Python Developer/29 - Segurança da Informação com Python/01_Por que Python.py
@@ -0,0 +1,13 @@
+# Porque o Python?
+
+# Linguagem simplista - fácil de programar
+# Menos linhas de códigos
+# Orientados a objetos
+# API's e bibliotecas prontas
+# Multiplataforma
+
+# Pequena curva de aprendizado, linguagem amplamente utilizada na area de
+# segurança da informação.
+
+# Ambiente Python Security
+# Prof utiliza PyCharm.
---
+++ Formação Python Developer/29 - Segurança da Informação com Python/02_O que é segurança da informação.py
@@ -0,0 +1,60 @@
+'''
+O que é dado?
+Pode ser uma representação simbólica, numérica ou textual qualquer.
+'''
+
+'''
+O que é informação?
+É o cunjunto ou junção de dados que fazem parte de um contexto ou fazem algum
+sentido.
+'''
+
+'''
+O que é segurança da Informação?
+Segurança da informação é um conjunto de práticas e tecnologias que visam
+proteger dados de acessos não autorizados, modificações, divulgação,
+interrupção e destruição. Em outras palavras, é um escudo que protege suas
+informações confidenciais, garantindo sua integridade, confidencialidade e
+disponibilidade.
+'''
+
+# Segurança da Informação
+# assegura que todos os dados de uma ou mais informações estejam sempre
+# confidencia, íntegros e disponíveis qualquer meio de comunicação.
+
+'''
+Porque Segurança?
+Piramide de Maslow
+"Fisiológicas": "Alimento, água, sono, sexo",
+"Segurança": "Proteção, estabilidade, ordem",
+"Social": "Amor, amizade, pertencimento",
+"Estima": "Autoestima, reconhecimento, status",
+"Autorealização": "Realização do potencial, crescimento pessoal"
+'''
+
+'''
+Pilares da segurança da informação:
+
+Confidencialidade: As informações devem ser acessíveis apenas por pessoas
+autorizadas. É como um segredo que só algumas pessoas podem conhecer.
+
+Integridade: As informações devem ser precisas e completas, sem alterações não
+autorizadas. Imagine um documento importante que não pode ser alterado por
+ninguém sem permissão.
+
+Disponibilidade: As informações devem estar acessíveis quando e onde forem
+necessárias. É como ter um arquivo importante sempre que você precisar.
+
+Autenticidade: A identidade de quem envia ou recebe as informações deve ser
+verificada. É como ter um passaporte que prova quem você é.
+
+Não repúdio: Impossibilidade de negar a autoria de uma informação. É como
+deixar um rastro digital que prova que você fez algo.
+
+Identificação: É o processo de estabelecer e verificar a identidade de um
+usuário ou dispositivo. É como apresentar um documento de identificação para provar quem você é.
+
+Autorização: É o processo de determinar quais recursos ou ações um usuário
+autenticado pode acessar. É como receber um crachá que te dá acesso a
+determinadas áreas de um prédio.
+'''
