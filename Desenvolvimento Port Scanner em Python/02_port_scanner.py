8d12857fffb4890050c998d9874cd45a22cea44a
Alex Sousa <lexusmem@gmail.com>
2024-07-28 13:15:04 -0300

    novas aulas port sccaner

--- Desenvolvimento Port Scanner em Python/02_port_scanner.py
+++ Desenvolvimento Port Scanner em Python/02_port_scanner.py
@@ -1,16 +1,43 @@
 import socket
 
-CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-CLIENT.settimeout(0.05)
+
+# função para verificar se é numero
+def eh_numero(digito):
+    return digito.isdigit()
+
 
 ip = input('Informe o host ou ip para verificar: ')
-port = int(input('Digite a porta a ser verificada: '))
 
-code = CLIENT.connect_ex((ip, port))
+n_portas_analise = 0
+ports = []
+count = 0
 
-if code == 0:
-    print('Porta Aberta.')
-    print(f'Retorno da conexão n° {code}.')
-else:
-    print('Porta Fechada.')
-    print(f'Retorno da conexão n° {code}.')
+n_portas_analise = input('Informe o número de portas a ser analisada: ')
+
+while not eh_numero(n_portas_analise):
+    print('Digite apenas números')
+    n_portas_analise = input('Informe o número de portas a ser analisada: ')
+
+while count < int(n_portas_analise):
+    num_porta = input('Digite a porta a ser verificada: ')
+    if eh_numero(num_porta):
+        ports.append(num_porta)
+        count += 1
+    else:
+        print('Digite apenas números.')
+        continue
+
+for port in ports:
+    porta = int(port)
+    CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    CLIENT.settimeout(0.05)
+    code = CLIENT.connect_ex((ip, porta))
+
+    if code == 0:
+        print(f'Porta n° {str(porta)} Aberta.')
+        print(f'Retorno da conexão n° {code}.\n')
+    else:
+        print(f'Porta n° {str(porta)} Fechada.')
+        print(f'Retorno da conexão n° {code}.\n')
+
+print('Scan Finalizado')
---
+++ Desenvolvimento Port Scanner em Python/03_dio_scanner.py
@@ -0,0 +1,61 @@
+import pprint
+import nmap
+
+scanner = nmap.PortScanner()
+
+
+# função para verificar se é numero
+def eh_numero(digito):
+    return digito.isdigit()
+
+
+# Função do Menu
+def menu():
+    return input('''
+Escolha o tipo de varredura a ser realizada
+1 - Varredura do Tipo SYN
+2 - Varredura do Tipo UDP
+3 - Varredura do Tipo Intensa
+                       
+Digite a opção escolhida: ''')
+
+
+print('Seja Bem Vindo ao LEXUS Scanner.\n')
+
+ip = input('Digite o I.P. e/ou Host a ser analisado: ')
+print(f'\nO ip/host informado foi {ip}.')
+
+tipo_varredura = menu()
+
+while not eh_numero(tipo_varredura):
+    print('Digite apenas números.')
+    tipo_varredura = menu()
+
+while int(tipo_varredura) < 1 or int(tipo_varredura) > 3:
+    print('Digite apenas números de 1 a 3.')
+    tipo_varredura = menu()
+
+print(f'A Varredura selecionada foi do tipo {tipo_varredura}.')
+print('Iniciando Varredura...')
+
+if tipo_varredura == '1':
+    print(f'Versão do Nmap: {scanner.nmap_version()}')
+    scanner.scan(ip, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
+    print(f'Status do IP/Host: {scanner[ip].state()}.')
+    print(scanner[ip].all_protocols())
+    print(f'Portas Abertas: {scanner[ip]['tcp'].keys()}')
+elif tipo_varredura == '2':
+    print(f'Versão do Nmap: {scanner.nmap_version()}')
+    scanner.scan(ip, '1-1024', '-v -sU')
+    print(scanner.scaninfo())
+    print(f'Status do IP/Host: {scanner[ip].state()}.')
+    print(scanner[ip].all_protocols())
+    print(f'Portas Abertas: {scanner[ip]['udp'].keys()}')
+else:
+    print(f'Versão do Nmap: {scanner.nmap_version()}')
+    scanner.scan(ip, '1-1024', '-v -sC')
+    print(scanner.scaninfo())
+    print(f'Status do IP/Host: {scanner[ip].state()}.')
+    print(scanner[ip].all_protocols())
+    print(f'Portas Abertas: {scanner[ip]['tcp'].keys()}')
