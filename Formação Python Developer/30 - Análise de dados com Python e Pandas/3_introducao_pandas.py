8988a2e42dfe27fd7982096a2155d89a04cb3c22
Alex Sousa <lexusmem@gmail.com>
2025-07-18 21:46:38 -0300

    Nova aula de pandas

--- Formação Python Developer/30 - Análise de dados com Python e Pandas/3_introducao_pandas.py
+++ Formação Python Developer/30 - Análise de dados com Python e Pandas/3_introducao_pandas.py
@@ -35,5 +35,56 @@ print(list(df.columns))
 # tipo de dados de cada coluna
 print(df.dtypes)
 
-# parei no minuto 14:25h
-# https://web.dio.me/lab/analise-de-dados-com-python-e-pandas/learning/c14c9169-c62d-4d8a-8ed8-04cbb51d5302
+# vizualizar as ultimas linhas do df
+print(df.tail())
+
+# descrição do conjunto de dados
+print(df.describe())
+
+# retornar valores de uma coluna
+print(df['Continente'])
+
+# retornar valores de uma coluna com dados únicos
+print(df['Continente'].unique())
+
+# filtros
+
+# filtrando coluna continente onde consta oceania
+oceania = df.loc[df['Continente'] == 'Oceania']
+print(oceania.head())
+
+# retornando os paises que constam no continente oceania
+paises_oceania = oceania['Pais'].unique()
+print(paises_oceania)
+
+dados_fiji = df.loc[df['Pais'] == 'Fiji']
+print(dados_fiji)
+
+paises_america = df.loc[df['Continente'] == 'Americas']['Pais'].unique()
+print(paises_america)
+
+dados_brazil = df.loc[df['Pais'] == 'Brazil']
+print(dados_brazil)
+
+# agrupamento de dados
+# agrupamento por continente
+# contagem de pais exclusivos nos continentes
+qtd_paises = df.groupby('Continente')['Pais'].nunique()
+print(qtd_paises)
+
+qtd_anos = df.groupby('Pais')['Anos'].nunique()
+print(qtd_anos)
+print(qtd_anos['Brazil'])
+print(qtd_anos.loc['Brazil'])
+
+# para cada ano, qual a expectativa de vida
+exp_por_ano = df.groupby('Anos')['Exp de Vida'].mean()
+print(exp_por_ano)
+
+# media da coluna
+media_pib = df['PIB'].mean()
+print(media_pib)
+
+# soma da coluna pib
+soma_pib = df['PIB'].sum()
+print(soma_pib)
